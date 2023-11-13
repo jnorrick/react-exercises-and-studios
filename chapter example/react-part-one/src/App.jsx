aafbd1477448dd44d7436202d624199023604b4d
Jessica Orrick <jessica.orrick@outlook.com>
2023-11-13 17:56:56 -0600

    Completed part 1 of exercise

---
+++ chapter example/react-part-one/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import Hello from './components/hello'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <Hello />
+      
+      </div>
+      
+    </>
+  )
+}
+
+export default App
--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Books I've Read This Year";
+   let book1 = "https://m.media-amazon.com/images/I/515AQlUdo4L.jpg";
+   let book2 = "https://images.gr-assets.com/books/1461769617l/26116474.jpg";
+   let book3 = "https://img1.od-cdn.com/ImageType-100/4575-1/%7B12585E65-86A3-46B9-9867-4A834EE92A0A%7DIMG100.JPG";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Holly by Stephen King" />
+         <img src={book2} alt="Head Full of Ghosts by Paul Tremblay" />
+         <img src={book3} alt="Episode Thirteen by Craig DiLouie" />
       </div>      
    );
 }
