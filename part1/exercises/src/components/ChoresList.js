5273623d94ca3d1120a2ec2f090a215d5465d117
Jessica Orrick <jessica.orrick@outlook.com>
2023-11-13 18:41:09 -0600

    updated chores list

--- part1/exercises/src/components/ChoresList.js
+++ part1/exercises/src/components/ChoresList.js
@@ -1,9 +1,12 @@
 export default function ChoresList () {
    return (
+   <>
+      <h2>Chores List</h2>
       <ol>
          <li>Complete Homework</li>
          <li>Take Car to Shop</li>
          <li>Take cats to vet</li>
       </ol>
+   </> 
    );
 }
