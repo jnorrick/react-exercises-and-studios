65b419d514a2a10dc9635fc32a66d5c32ec61c57
Jessica Orrick <jessica.orrick@outlook.com>
2023-11-16 14:21:00 -0600

    completed exercise

--- part2/exercises-2/src/components/Profile.js
+++ part2/exercises-2/src/components/Profile.js
@@ -1,14 +1,28 @@
+import oceans from './oceans.json';
 import './styles.css';
 import Button from './Button';
 
 
+const listItem = oceans.map(ocean => 
+   <div key={ocean.id} className="profile">
+      <img src={ocean.image} alt={ocean.name} className="img" />
+      <h1>{ocean.name}</h1>
+      <h3>Fun Facts</h3>
+      <ul>
+         <li>{ocean.fact1}</li>
+         <li>{ocean.fact2}</li>
+         <li>{ocean.fact3}</li>
+      </ul>
+      <Button />
+   </div>
+      );
+
 function Profile()
 {
    return(
-      <>
-         <h3>Coming Soon! Profiles of Ocean creatures</h3>
-         <Button />
-      </>  
+         <ul>
+         {listItem}
+         </ul>
    );
 }
 
