6f48bc246d7b437502d5b700866b30be6f6c8a78
Jessica Orrick <jessica.orrick@outlook.com>
2023-11-19 13:03:27 -0600

    exercise update

--- part3/exercises/src/components/Introduction.jsx
+++ part3/exercises/src/components/Introduction.jsx
@@ -1,11 +1,11 @@
 export default function HobbyIntroduction () {
    return(
       <div>
-         <h2>3 Fun Facts About Needlepoint</h2>
+         <h2>3 Fun Facts About Subversive Cross Stitch</h2>
          <ol>
-            <li>Needlepoint uses a specialized canvas as it's base.</li>
-            <li>The most basic stitch is called a tent stitch.</li>
-            <li>Needlepoint uses a lot of different kinds of fibers.</li>
+            <li>Cross Stitch is a great way to relax.</li>
+            <li>Cross Stitch is easy to learn, difficult to master.</li>
+            <li>Subversive Cross Stitch allows you to display your personality!</li>
          </ol>
       </div>
    );
